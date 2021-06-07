907f31387f4f61843061f2c15e438d8a2ab00796
Atta Jirofty <jirofty76@gmail.com>
2021-06-07 16:32:48 +0430

    First Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1500);
+}
+
+function showPage() {
+  document.getElementById("box").style.display = "none";
+  document.getElementById("after-load").style.display = "block";
+}
