b9e587cf44764fa5fbcafb757c8591ae8708cb1a
Atta <jirofty76@gmail.com>
2021-08-12 01:13:26 +0430

    Fixed some bugs - Clean code

--- js/script.js
+++ js/script.js
@@ -1,21 +1,7 @@
+var timer;
 
-if ($(".text-slider").length == 1) {
-  var typed_strings = $(".text-slider-items").text();
-  var typed = new Typed(".text-slider", {
-      strings: typed_strings.split(", "),
-      typeSpeed: 30,
-      loop: true,
-      backDelay: 2000,
-      backSpeed: 100,
-  });
-}
-
-
-
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 1500);
+function loading() {
+  timer = setTimeout(showPage, 1500);
 }
 
 function showPage() {
