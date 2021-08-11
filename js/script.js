7576250e79ff87d8fe9205df57f38605efd1b792
Atta <jirofty76@gmail.com>
2021-08-11 23:44:36 +0430

    Some basic changes !

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 
-  
+if ($(".text-slider").length == 1) {
+  var typed_strings = $(".text-slider-items").text();
+  var typed = new Typed(".text-slider", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 30,
+      loop: true,
+      backDelay: 2000,
+      backSpeed: 100,
+  });
+}
+
+
+
 var myVar;
 
 function myFunction() {
